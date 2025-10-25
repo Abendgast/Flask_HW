ec716cbbadf97d9174089491d908ae8cb32c2d53
Abendgast <s.prilepsky0806@gmail.com>
2025-10-25 14:47:01 +0300

    Add users blueprint with templates and styles

--- lab3/project/app/__init__.py
+++ lab3/project/app/__init__.py
@@ -2,4 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+from app.users import users_bp
+app.register_blueprint(users_bp)
+
 from app import views
---
+++ lab3/project/app/users/views.py
@@ -0,0 +1,11 @@
+from app.users import users_bp
+from flask import render_template, redirect, url_for
+
+@users_bp.route('/hi/<name>')
+def greetings(name):
+    age = request.args.get('age', 'невідомий')
+    return render_template('users/hi.html', name=name.upper(), age=age)
+
+@users_bp.route('/admin')
+def admin():
+    return redirect(url_for('users.greetings', name='Administrator', age=45))
